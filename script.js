4326d3f4ebbb78adc0db1c11d6ac2888d3fb3df3
Dhruv Jain <dhruvjainstuff@gmail.com>
2022-01-09 22:03:19 +0400

    Working Prototype as Base

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
-    let urlone = tabs[0].url;
-    const paraElement = document.getElementById('para');
-    paraElement.innerHTML = urlone;
-});
+chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
+  let urlone = tabs[0].url;
+  const paraElement = document.getElementById('para');
+  paraElement.innerHTML = urlone;
+
+  fetch('http://127.0.0.1:5000/api?skills=Machine%20Learning').then(r => r.text()).then(result => {
+    paraElement.innerHTML = result;
+  })
+})
