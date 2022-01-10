b1fdfdb6000f632d1e80edc5f5ed976b8320cb47
Aditya Saxena <aditya.uni07@gmail.com>
2022-01-10 21:22:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
-  let urlone = tabs[0].url;
-  const paraElement = document.getElementById('para');
-  paraElement.innerHTML = urlone;
-
-  fetch('http://127.0.0.1:5000/phase1',{ method : 'post', body : urlone}).then(r => r.json()).then(result => {
-    paraElement.innerHTML = result;
+    let urlone = tabs[0].url;
+    const paraElement = document.getElementById('para');
+    paraElement.innerHTML = urlone;
+    urlone = 'http://127.0.0.1:5000/phase1/' + urlone
+  
+    fetch(urlone).then((response) => { return response.json(); })
   })
-})
