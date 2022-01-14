50e0cf6cb869d793be4feb0d7d341ad57af401f1
Dhruv Jain <46486943+datDhruvJain@users.noreply.github.com>
2022-01-14 15:39:26 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,36 @@
-chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
-    let urlone = tabs[0].url;
-    const paraElement = document.getElementById('para');
-    paraElement.innerHTML = urlone;
-    urlone = 'http://127.0.0.1:5000/phase1/' + urlone
-  
-    fetch(urlone).then((response) => { return response.json(); })
-  })
+let sendURL;
+
+async function getCurrentTab() {
+  let queryOptions = { active: true, currentWindow: true };
+  let [tab] = await chrome.tabs.query(queryOptions);
+  return tab.url
+}
+
+async function mainFunc() {
+  sendURL = await getCurrentTab();
+  const paraElement = document.getElementById('para');
+  paraElement.innerHTML = sendURL;
+  sendURL = btoa(sendURL)
+  sendURL = 'http://127.0.0.1:5000/' + sendURL
+
+  let ans = await fetch(sendURL)
+    .then(
+      function (response) {
+        if (response.status !== 200) {
+          console.log('Looks like there was a problem. Status Code: ' +
+            response.status);
+          return;
+        }
+
+        // Examine the text in the response
+        response.json().then(function (data) {
+          console.log(data)
+        });
+      }
+    )
+    .catch(function (err) {
+      console.log('Fetch Error :-S', err);
+    });
+}
+
+mainFunc()
