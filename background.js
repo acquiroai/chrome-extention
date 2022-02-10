6e9852b5f824ba20ff4a042067ea014591f93d0b
Aditya Saxena <aditya.uni07@gmail.com>
2022-02-10 12:57:35 +0530

    Update background.js

--- background.js
+++ background.js
@@ -6,19 +6,19 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
    // how to fetch tab url using activeInfo.tabid
    chrome.tabs.get(activeInfo.tabId, function(tab){
        let getURL = tab.url;
-       if (getURL.substring(12,23) == "simplyhired"){ //simplyhired getURL.substring(12,23)
+       if (getURL.substring(12,36) == "simplyhired.co.in/search"){ //simplyhired getURL.substring(12,23)
               chrome.action.setIcon({path: { "16": "icon16.png", "48": "icon48.png", "128": "icon128.png" }, tabId: activeInfo.tabId});
        } 
 
-       else if (getURL.substring(12,18) == "naukri"){
+       else if (getURL.substring(12,35) == "naukri.com/job-listings"){
         chrome.action.setIcon({path: { "16": "icon16.png", "48": "icon48.png", "128": "icon128.png" }, tabId: activeInfo.tabId});
     } 
 
-       else if (getURL.substring(12,19) == "iimjobs"){
+       else if (getURL.substring(12,25) == "iimjobs.com/j"){
         chrome.action.setIcon({path: { "16": "icon16.png", "48": "icon48.png", "128": "icon128.png" }, tabId: activeInfo.tabId});
     }
 
-       else if (getURL.substring(12,24) == "monsterindia") {
+       else if (getURL.substring(12,32) == "monsterindia.com/job") {
         chrome.action.setIcon({path: { "16": "icon16.png", "48": "icon48.png", "128": "icon128.png" }, tabId: activeInfo.tabId});
     }
 
